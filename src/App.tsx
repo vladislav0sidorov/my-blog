ca099a6a47c44c68198169293c6b795fd3267a37
Vladislav <vladislav0sidorov.gmail.com>
2023-02-02 23:40:40 +0400

    added routing and react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from '../public/components/Counter';
 
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { HomePageAsync } from './pages/HomePage/HomePage.async';
 
 export const App = () => {
   return (
     <div className="app">
+      <Link to="/">Домой</Link>
+      <Link to="about">О себе</Link>
+      <Suspense fallback={<div>LOADING...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePageAsync />}></Route>
+          <Route path="about" element={<AboutPageAsync />}></Route>
+        </Routes>
+      </Suspense>
       <Counter />
     </div>
   );
