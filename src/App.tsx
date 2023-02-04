79fae29afe67c6f4f24714e5b82300f43223421b
Vladislav <vladislav0sidorov.gmail.com>
2023-02-04 14:31:18 +0400

    Create ClassNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import { Counter } from '../public/components/Counter';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { HomePageAsync } from './pages/HomePage/HomePage.async';
 import { useTheme } from './theme/useTheme';
+import { ClassNames } from './helpers/ClassNames/ClassNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={ClassNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>СВИЧ ТЕМЫ</button>
       <Link to="/">Домой</Link>
       <Link to="about">О себе</Link>
---
+++ src/helpers/ClassNames/ClassNames.ts
@@ -0,0 +1,11 @@
+type Mods = Record<string, boolean | string>;
+//* cls - class name
+export function ClassNames(cls: string, mods: Mods, additionals: string[]): string {
+  return [
+    cls,
+    ...additionals,
+    Object.entries(mods)
+      .filter(([cls, value]) => Boolean(value))
+      .map(([cls]) => cls),
+  ].join(' ');
+}
