e69bc2bf97f19e5993786d2c5f4193178fb50fc9
Vladislav <vladislav0sidorov.gmail.com>
2023-02-14 00:57:25 +0400

    added error boundary react

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,11 +1,12 @@
+import { BugButton } from 'app/providers/ErrorBoundery';
 import { useTranslation } from 'react-i18next';
-import { PageLoader } from 'widgets/PageLoader';
 
 const HomePage = () => {
   const { t } = useTranslation('home');
 
   return (
     <div>
+      <BugButton />
       {t('Домашняя страница')}
     </div>
   );
