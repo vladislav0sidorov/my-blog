ced85609edfac77e44657133f5598610c5dc78eb
Vladislav <vladislav0sidorov.gmail.com>
2023-02-16 00:26:46 +0400

    added react testing library, added test for button and sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+
+import { Sidebar } from './Sidebar';
+
+describe('Sidebar', () => {
+  test('Render sidebar', () => {
+    renderWithTranslation(<Sidebar />);
+    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+  });
+  test('Sidebar toggle', () => {
+    renderWithTranslation(<Sidebar />);
+    const toggleBtn = screen.getByTestId('sidebar-toggle');
+    expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+    fireEvent.click(toggleBtn);
+    expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+  });
+});
