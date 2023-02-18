e2a9043e478d2a41e86fe9d027f69ef053175795
Vladislav <vladislav0sidorov.gmail.com>
2023-02-18 15:51:39 +0400

    added translation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,13 +6,16 @@ import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
 
 const App = () => {
   const { theme } = useTheme();
+  const { t } = useTranslation();
 
   return (
+
     <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback={<div>Загрузка языка</div>}>
+      <Suspense fallback={<div>{t('Загрузка языка')}</div>}>
         <Navbar />
         <div className="content-page">
           <Sidebar />
