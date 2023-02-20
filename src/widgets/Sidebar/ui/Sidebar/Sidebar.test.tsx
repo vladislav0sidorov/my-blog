e4223f8162795db1de6be935921c7095b62bb4a6
Vladislav <vladislav0sidorov.gmail.com>
2023-02-20 11:43:42 +0400

    added reg-cli and fixed problems

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,15 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { fireEvent, screen } from '@testing-library/react';
+import { componentRender } from 'shared/config/tests/componentRender/componentRender';
 
 import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
   test('Render sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('Sidebar toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
