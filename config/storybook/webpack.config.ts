ec91031195266935d60cd600a5daba203c4bc9df
Sidorov Vladislav <vladislav0sidorov@gmail.com>
2023-03-06 12:13:44 +0400

    fixed problems with linter

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -15,7 +15,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
   config.resolve?.extensions?.push('.ts', '.tsx');
 
   // eslint-disable-next-line no-param-reassign
-  //@ts-ignore
+  // @ts-ignore
   config!.module!.rules = config!.module!.rules.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -3,7 +3,6 @@ import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 import { loginByUsername } from './loginByUsername';
 
 describe('loginByUsername.test', () => {
-
   test('success login', async () => {
     const userValue: any = { username: '123', id: '1' };
 
