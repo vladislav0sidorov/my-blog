605464a934a1c346e7cafeaf40d532ca1d2295cc
Sidorov Vladislav <vladislav0sidorov@gmail.com>
2023-04-16 14:46:32 +0400

    fix stylelint, add copy-webpack-plugin and include it

--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/ClassNames/ClassNames';
 import { Page } from 'widgets/Page';
 import { useParams } from 'react-router-dom';
-import cls from './ArticleEditPage.module.scss';
+// import cls from './ArticleEditPage.module.scss';
 
 interface ArticleEditPageProps {
   className?: string;
@@ -14,7 +14,7 @@ const ArticleEditPage: FC<ArticleEditPageProps> = React.memo((props) => {
   const { t } = useTranslation();
   const { id } = useParams<{ id: string }>();
   const isEdit = Boolean(id);
-  return <Page className={classNames(cls.articleEditPage, {}, [className])}>{isEdit ? t('Редактирование страницы') : t('Создание новой страницы')}</Page>;
+  return <Page className={classNames('', {}, [className])}>{isEdit ? t('Редактирование страницы') : t('Создание новой страницы')}</Page>;
 });
 
 export default ArticleEditPage;
