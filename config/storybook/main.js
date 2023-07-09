e1541ecf825dfb50ea1a7fd54c9af3cb5ac6f068
Vladislav <vladislav0sidorov@gmail.com>
2023-07-09 19:27:13 +0400

    update path for storybook config

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -12,7 +12,7 @@ module.exports = {
     'storybook-addon-mock',
     'storybook-addon-themes',
   ],
-  staticDirs: ['../../src/shared/icons/*.@(svg)'],
+  staticDirs: ['../../src/shared/icons/'],
   framework: '@storybook/react',
   core: {
     builder: 'webpack5',
