f80a7de71eb348802869f862a59873cb13655006
Vladislav <vladislav0sidorov@gmail.com>
2023-08-04 22:05:00 +0400

    update storybook version, disable screenshot testing

---
+++ src/features/ScrollRestoration/ui/ScrollRestoration.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ScrollRestoration } from './ScrollRestoration';
+
+export default {
+  title: 'features/ScrollRestoration',
+  component: ScrollRestoration,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof ScrollRestoration>;
+
+const Template: ComponentStory<typeof ScrollRestoration> = (args) => (
+  <ScrollRestoration {...args} />
+);
+
+export const Normal = Template.bind({});
+Normal.args = {};
